abad3e019deec6a0738e2fe8c2248d7675414b86
Jeffking Sterile <jsterile@solutions.ht>
2024-09-04 22:02:15 -0400

    CorrectionAffichagePhoto

--- app/src/main/java/org/mbds/unice/github/data/api/FakeApiServiceGenerator.kt
+++ app/src/main/java/org/mbds/unice/github/data/api/FakeApiServiceGenerator.kt
@@ -7,26 +7,26 @@ object FakeApiServiceGenerator {
 
     @JvmField
     var FAKE_USERS = mutableListOf(
-        User("001", "Jake", "https://api.adorable.io/AVATARS/512/1.png"),
-        User("002", "Paul", "https://api.adorable.io/AVATARS/512/2.png"),
-        User("003", "Phil", "https://api.adorable.io/AVATARS/512/3.png"),
-        User("004", "Guillaume", "https://api.adorable.io/AVATARS/512/4.png"),
+        User("001", "Jake", "https://www.freeiconspng.com/uploads/person-icon-red-31.png"),
+        User("002", "Paul", "https://www.seekpng.com/png/detail/17-176376_person-free-download-and-person-icon-png.png"),
+        User("003", "Phil", "https://www.vhv.rs/dpng/f/10-101667_person-icon-png.png"),
+        User("004", "Guillaume", "https://www.freeiconspng.com/uploads/person-icon-red-31.png"),
         User("005", "Francis", "https://api.adorable.io/AVATARS/512/5.png"),
-        User("006", "George", "https://api.adorable.io/AVATARS/512/6.png"),
+        User("006", "George", "https://www.freeiconspng.com/uploads/person-icon-red-31.png"),
         User("007", "Louis", "https://api.adorable.io/AVATARS/512/7.png"),
-        User("008", "Mateo", "https://api.adorable.io/AVATARS/512/8.png"),
+        User("008", "Mateo", "https://www.freeiconspng.com/uploads/person-icon-red-31.png"),
         User("009", "April", "https://api.adorable.io/AVATARS/512/9.png"),
-        User("010", "Louise", "https://api.adorable.io/AVATARS/512/10.png"),
+        User("010", "Louise", "https://www.seekpng.com/png/detail/17-176376_person-free-download-and-person-icon-png.png"),
         User("011", "Elodie", "https://api.adorable.io/AVATARS/512/11.png"),
-        User("012", "Helene", "https://api.adorable.io/AVATARS/512/12.png"),
-        User("013", "Fanny", "https://api.adorable.io/AVATARS/512/13.png"),
+        User("012", "Helene", "https://www.freeiconspng.com/uploads/person-icon-red-31.png"),
+        User("013", "Fanny", "https://www.seekpng.com/png/detail/17-176376_person-free-download-and-person-icon-png.png"),
         User("014", "Laura", "https://api.adorable.io/AVATARS/512/14.png"),
-        User("015", "Gertrude", "https://api.adorable.io/AVATARS/512/15.png"),
-        User("016", "Chloé", "https://api.adorable.io/AVATARS/512/16.png"),
-        User("017", "April", "https://api.adorable.io/AVATARS/512/17.png"),
-        User("018", "Marie", "https://api.adorable.io/AVATARS/512/18.png"),
-        User("019", "Henri", "https://api.adorable.io/AVATARS/512/19.png"),
-        User("020", "Rémi", "https://api.adorable.io/AVATARS/512/20.png")
+        User("015", "Gertrude", "https://www.freeiconspng.com/uploads/person-icon-red-31.png"),
+        User("016", "Chloé", "https://www.seekpng.com/png/detail/17-176376_person-free-download-and-person-icon-png.png"),
+        User("017", "April", "https://www.freeiconspng.com/uploads/person-icon-red-31.png"),
+        User("018", "Marie", "https://www.seekpng.com/png/detail/17-176376_person-free-download-and-person-icon-png.png"),
+        User("019", "Henri", "https://www.freeiconspng.com/uploads/person-icon-red-31.png"),
+        User("020", "Rémi", "https://www.freeiconspng.com/uploads/person-icon-red-31.png")
     )
 
     @JvmField
--- app/src/main/java/org/mbds/unice/github/ui/users/ListUserViewHolder.kt
+++ app/src/main/java/org/mbds/unice/github/ui/users/ListUserViewHolder.kt
@@ -16,11 +16,10 @@ class ListUserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val deleteButton: ImageButton = itemView.findViewById(R.id.item_list_user_delete_button)
 
     fun bind(user: User, callback: UserListAdapter.Listener) {
-        // TODO : Utiliser une librairie pour charger l'image (glide, coil, )
-        Glide.with(this)
-            .load("https://example.com/image.jpg")
+        var imageurl : String = user.avatarUrl
+        Glide.with(itemView)
+            .load(imageurl)
             .into(avatar)
-
         username.text = user.login
         deleteButton.setOnClickListener { callback.onClickDelete(user) }
     }
